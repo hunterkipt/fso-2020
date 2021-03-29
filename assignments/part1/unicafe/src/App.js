9479b2ca7a9d43f47ddafd6e1b3722b8427708b7
Hunter Kippen <hunterkipt@gmail.com>
2021-03-29 18:24:02 -0400

    unicafe: Ex 1.7

--- assignments/part1/unicafe/src/App.js
+++ assignments/part1/unicafe/src/App.js
@@ -24,20 +24,41 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
+
+  const average = total !== 0 ? (good - bad) / total : total
+  const positive = total !== 0 ? good / total * 100 : 100
   return (
     <>
     <h1>Give Feedback</h1>
     <div>
-      <Button handleClick={() => setGood(good + 1)} text='Good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='Bad' />
+      <Button handleClick={handleGood} text='Good' />
+      <Button handleClick={handleNeutral} text='Neutral' />
+      <Button handleClick={handleBad} text='Bad' />
     </div>
     <h1>Statistics</h1>
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive + '%'}</p>
     </div>
     </>
   )
